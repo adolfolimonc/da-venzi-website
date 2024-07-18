e4a85ed0c26d6cc038fcb4c6214b71a9b831626f
adolfolimonc <adolfolimonc@outlook.com>
2024-07-17 23:48:16 -0700

    correction contact

--- reservation.php
+++ reservation.php
@@ -1,18 +1,20 @@
 ﻿<?php
 
-$myemail = 'you@yoursite.com';
+$myemail = 'adolfolimonc@gmail.com';
+$subject = 'Nueva reservacion';
+
 
 $date    = $_POST['date'];
 $email   = $_POST['email'];
-$subject = $_POST['subject'];
 $time    = $_POST['time'];
 $persons = $_POST['persons'];
 
-$to            = $myemail;
-$email_subject = "Table reservation";
-$email_body    = "\n Date: $date \n Email: $email \n Subject: Table reservation \n Time: $time \n Persons: $persons";
-$headers       = "From: $email";
+function sendEmail($subject,$myemail,$me)
+// $to            = $myemail;
+// $email_subject = "Table reservation";
+// $email_body    = "\n Date: $date \n Email: $email \n Subject: Table reservation \n Time: $time \n People: $persons";
+// $headers       = "From: $email";
  
-mail($to, $email_subject, $email_body, $headers);
+// mail($to, $email_subject, $email_body, $headers);
 
 ?>
